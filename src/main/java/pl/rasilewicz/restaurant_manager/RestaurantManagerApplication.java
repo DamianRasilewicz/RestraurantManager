8af64a2977dc07fba5bbb668b6a69e6db5941c7b
DamianRasilewicz <rasil754@gmail.com>
2021-01-18 21:03:19 +0100

    new project and configured .gitignore

---
+++ src/main/java/pl/rasilewicz/restaurant_manager/RestaurantManagerApplication.java
@@ -0,0 +1,13 @@
+package pl.rasilewicz.restaurant_manager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestaurantManagerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RestaurantManagerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/pl/rasilewicz/restaurant_manager/RestaurantManagerApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.rasilewicz.restaurant_manager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestaurantManagerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
