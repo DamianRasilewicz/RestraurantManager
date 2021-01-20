8431eabe43522332742b9d31385dde7c64b7b30d
DamianRasilewicz <rasil754@gmail.com>
2021-01-20 17:27:52 +0100

    TypeOfProducts service added

---
+++ src/main/java/pl/rasilewicz/restaurant_manager/services/TypeOfProductsService.java
@@ -0,0 +1,9 @@
+package pl.rasilewicz.restaurant_manager.services;
+
+import pl.rasilewicz.restaurant_manager.entities.TypeOfProduct;
+import java.util.List;
+
+public interface TypeOfProductsService {
+
+    List<TypeOfProduct> findAllTypesOfProduct();
+}
---
+++ src/main/java/pl/rasilewicz/restaurant_manager/services/TypeOfProductsServiceImpl.java
@@ -0,0 +1,22 @@
+package pl.rasilewicz.restaurant_manager.services;
+
+import org.springframework.stereotype.Service;
+import pl.rasilewicz.restaurant_manager.entities.TypeOfProduct;
+import pl.rasilewicz.restaurant_manager.repositories.TypeOfProductRepository;
+
+import java.util.List;
+
+@Service
+public class TypeOfProductsServiceImpl implements TypeOfProductsService{
+
+    private final TypeOfProductRepository typeOfProductRepository;
+
+    public TypeOfProductsServiceImpl(TypeOfProductRepository typeOfProductRepository) {
+        this.typeOfProductRepository = typeOfProductRepository;
+    }
+
+    @Override
+    public List<TypeOfProduct> findAllTypesOfProduct() {
+        return typeOfProductRepository.findAllTypesOfProduct();
+    }
+}
