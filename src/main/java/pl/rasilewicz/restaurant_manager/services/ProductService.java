2e620e8ed9c70fd8f10a37667a5538e11a208ee4
DamianRasilewicz <rasil754@gmail.com>
2021-01-20 09:16:04 +0100

    Product service added

---
+++ src/main/java/pl/rasilewicz/restaurant_manager/services/ProductService.java
@@ -0,0 +1,11 @@
+package pl.rasilewicz.restaurant_manager.services;
+
+import pl.rasilewicz.restaurant_manager.entities.Product;
+import java.util.List;
+
+public interface ProductService {
+
+    List<Product> findProductsByType(Long typeId);
+
+    List<Product> findAllProducts();
+}
---
+++ src/main/java/pl/rasilewicz/restaurant_manager/services/ProductServiceImpl.java
@@ -0,0 +1,29 @@
+package pl.rasilewicz.restaurant_manager.services;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import pl.rasilewicz.restaurant_manager.entities.Product;
+import pl.rasilewicz.restaurant_manager.repositories.ProductRepository;
+
+import java.util.List;
+
+@Service
+public class ProductServiceImpl implements ProductService{
+
+    private final ProductRepository productRepository;
+
+    @Autowired
+    public ProductServiceImpl(ProductRepository productRepository){
+        this.productRepository = productRepository;
+    }
+
+    @Override
+    public List<Product> findProductsByType(Long typeId) {
+        return productRepository.findProductsByType(typeId);
+    }
+
+    @Override
+    public List<Product> findAllProducts() {
+        return productRepository.findAllProducts();
+    }
+}
