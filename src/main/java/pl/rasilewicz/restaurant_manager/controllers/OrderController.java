9a7bbe78c595c52ec8a669c6fe318e4c97974a4a
DamianRasilewicz <rasil754@gmail.com>
2021-01-23 11:04:40 +0100

    Deleting product in order works also number of products in order and cost of order updating after delete product works

--- src/main/java/pl/rasilewicz/restaurant_manager/controllers/OrderController.java
+++ src/main/java/pl/rasilewicz/restaurant_manager/controllers/OrderController.java
@@ -3,7 +3,9 @@ package pl.rasilewicz.restaurant_manager.controllers;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import pl.rasilewicz.restaurant_manager.entities.Addition;
 import pl.rasilewicz.restaurant_manager.entities.Order;
 import pl.rasilewicz.restaurant_manager.entities.Product;
 import pl.rasilewicz.restaurant_manager.services.OrderServiceImpl;
@@ -47,6 +49,43 @@ public class OrderController {
         return "redirect:/";
     }
 
+    @GetMapping("/order/product/delete")
+    public String orderProductDelete (@RequestParam int index, HttpSession session){
+
+        Order order = (Order) session.getAttribute("order");
+
+        List <Product> productsInOrder = order.getProducts();
+        Product deletingProduct = productsInOrder.get(index);
+
+        double costOfRemovingProduct = 0.00;
+        if (deletingProduct.getAdditions() == null){
+            costOfRemovingProduct = costOfRemovingProduct + deletingProduct.getPrice();
+        }else {
+
+            for (Addition addition : deletingProduct.getAdditions()) {
+                costOfRemovingProduct = costOfRemovingProduct + addition.getPrice();
+            }
+            costOfRemovingProduct = costOfRemovingProduct + deletingProduct.getPrice();
+        }
+
+        double orderCost = order.getOrderCost();
+        orderCost = orderCost - costOfRemovingProduct;
+        order.setOrderCost(orderCost);
+
+        int numberOfProduct = order.getNumberOfProducts();
+        numberOfProduct = numberOfProduct - 1;
+        order.setNumberOfProducts(numberOfProduct);
+        order.setNumberOfProducts(numberOfProduct);
+
+        productsInOrder.remove(index);
+        order.setProducts(productsInOrder);
+
+        session.setAttribute("order", order);
+
+        return "redirect:/order/basket";
+    }
+
+
     @GetMapping("/order/submit")
     public String orderSubmitForm (HttpSession session, Model model){
 
@@ -54,5 +93,11 @@ public class OrderController {
         model.addAttribute("order", order);
 
         return "mainPage/orderSubmitForm";
+    }
+
+    @PostMapping("/order/submit")
+    public String orderSubmitted (){
+
+        return "mainPage/orderSubmitted";
     }
 }
